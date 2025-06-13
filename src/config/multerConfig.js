590d3ab28c726ceae574ef004fbd191e31099d64
Campione <80720402+alfrecampione@users.noreply.github.com>
2025-06-13 11:07:54 -0400

    Content resize

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -25,7 +25,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
     storage,
-    limits: { fileSize: 10 * 1024 * 1024 },
+    limits: { fileSize: 2 * 1024 * 1024 },
     fileFilter: (req, file, cb) => {
         const allowed = /jpeg|jpg|png|pdf|doc|docx/;
         const isValid = allowed.test(path.extname(file.originalname).toLowerCase()) &&
