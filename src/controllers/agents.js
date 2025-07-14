2d1f2d04513a4e45f1f8605a97f66f41b62199e9
Campione <80720402+alfrecampione@users.noreply.github.com>
2025-07-14 15:05:12 -0400

    set order for agents

--- src/controllers/agents.js
+++ src/controllers/agents.js
@@ -3,13 +3,12 @@ import { prisma } from "../config/dbConfig.js";
 const renderAgents = async (req, res) => {
   const user = req.user;
 
-  const registeredUsersRaw = await prisma.$queryRaw`
-        SELECT * FROM "User" u 
-        LEFT JOIN "PersonalInfo" pi ON u.user_id = pi."userId"
-    `;
-
-  // Convert to plain JSON array (if not already)
-  const registeredUsers = JSON.parse(JSON.stringify(registeredUsersRaw));
+  const registeredUsers = await prisma.user.findMany({
+    orderBy: [
+      { display_name: 'asc' },
+      { email: 'asc' }
+    ]
+  });
 
   res.render("agents", { user, registeredUsers });
 };
