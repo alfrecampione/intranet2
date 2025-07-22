8a26816d5f39d43b063956430b1cccf295d4b151
Campione <80720402+alfrecampione@users.noreply.github.com>
2025-07-22 02:09:16 -0400

    profile fixed

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -3,19 +3,27 @@ import { prisma } from "../config/dbConfig.js";
 const renderProfile = async (req, res) => {
     const user = req.user;
 
+    const userId = req.params.id ?? user.user_id
+
     const profile = await prisma.user.findUnique({
         where: {
-            user_id: user_id
+            user_id: userId
         }
     });
 
     const personalInfo = await prisma.personalInfo.findUnique({
         where: {
-            userId: user_id
+            userId: userId
         }
     });
 
-    res.render("profile", { user: user, profile: profile, personalInfo: personalInfo });
+    const contactInfo = await prisma.contactInfo.findUnique({
+        where: {
+            userId: userId
+        }
+    });
+
+    res.render("profile", { user: user, profile: profile, personalInfo: personalInfo, contactInfo: contactInfo });
 }
 
 
