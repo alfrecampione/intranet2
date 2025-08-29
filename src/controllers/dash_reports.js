186b767f1137ee5e2199c5cb89693c51232fd2bd
Campione <alfredo.montero@estudiantes.matcom.uh.cu>
2025-08-29 12:32:02 -0400

    log removed

--- src/controllers/dash_reports.js
+++ src/controllers/dash_reports.js
@@ -37,7 +37,6 @@ const renderDashboard = async (req, res) => {
     let user = req.user;
 
     if (user && user.personalInfo?.contactType?.toLowerCase() === 'business') {
-      console.log('Business user detected for dashboard');
       const agency = await prisma.agency.findUnique({ where: { owner: user.user_id } });
       if (agency) {
         const allAgencyIds = await getAllAgencyIds(agency.id, prisma);
@@ -69,7 +68,6 @@ const renderDashboard = async (req, res) => {
         companies = await prisma.company.findMany();
         states = [...new Set(userCompanyStateData.map(d => d.state))];
 
-        console.log('Companies and states loaded for business user: ', companies.length, states.length);
       } else {
         // No agency found, show nothing
         companies = [];
