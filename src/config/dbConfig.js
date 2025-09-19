1a32f3e550a2e9c0837e8b5e6166dfb4d5398e7b
Campione <alfredo.montero@estudiantes.matcom.uh.cu>
2025-09-19 10:08:40 -0400

    SQL url work change

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const PgStore = connectPgSimple(session);
 
-const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const connectionString = process.env.DATABASE_URL
 
 let sessionStore;
 let pool;
