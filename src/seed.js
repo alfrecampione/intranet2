9105898db2fd9e7ecc2947845f48f53da54352d8
Campione <80720402+alfrecampione@users.noreply.github.com>
2025-10-06 05:28:42 -0400

    seed error

--- src/seed.js
+++ src/seed.js
@@ -1,38 +1,54 @@
 import { prisma } from "./config/dbConfig.js";
 
 const RIGHTS = ["read", "write"];
-
 const ALLOWED_AGENTS = ["goldenhealth@goldentrust.com"];
 
-let allRights = [];
+async function main() {
+    let allRights = [];
 
-for (const right of RIGHTS) {
-    const createdRight = await prisma.right.upsert({
-        where: { name: right },
-        update: {},
-        create: { name: right }
+    for (const right of RIGHTS) {
+        const createdRight = await prisma.right.upsert({
+            where: { name: right },
+            update: {},
+            create: { name: right }
+        });
+        allRights.push(createdRight);
+    }
+
+    for (const agent of ALLOWED_AGENTS) {
+        await prisma.allowedAgents.upsert({
+            where: { email: agent },
+            update: {},
+            create: { email: agent }
+        });
+    }
+
+    const agent = await prisma.allowedAgents.findUnique({
+        where: { email: "goldenhealth@goldentrust.com" },
     });
-    allRights.push(createdRight);
+
+    for (const right of allRights) {
+        await prisma.agentRights.upsert({
+            where: {
+                idAgent_idRight: {
+                    idAgent: agent.id,
+                    idRight: right.id
+                }
+            },
+            update: {},
+            create: {
+                idAgent: agent.id,
+                idRight: right.id
+            }
+        });
+    }
 }
 
-for (const agent of ALLOWED_AGENTS) {
-    await prisma.allowedAgents.upsert({
-        where: { email: agent },
-        update: {},
-        create: { email: agent }
+main()
+    .catch(e => {
+        console.error(e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
     });
-}
-
-const agent = await prisma.allowedAgents.findUnique({
-    where: { email: "goldenhealth@goldentrust.com" },
-});
-
-for (const right of allRights) {
-    await prisma.agentRights.create({
-        where: { idAgent: agent.id },
-        data: {
-            idAgent: agent.id,
-            idRight: right.id
-        }
-    });
-}
