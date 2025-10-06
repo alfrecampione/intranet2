c5d620cf02a8ad48324d1869dde20a6e0094a5bb
Campione <80720402+alfrecampione@users.noreply.github.com>
2025-10-06 05:31:40 -0400

    seed error 2

--- src/seed.js
+++ src/seed.js
@@ -7,11 +7,10 @@ async function main() {
     let allRights = [];
 
     for (const right of RIGHTS) {
-        const createdRight = await prisma.right.upsert({
-            where: { name: right },
-            update: {},
-            create: { name: right }
-        });
+        let createdRight = await prisma.right.findUnique({ where: { name: right } });
+        if (!createdRight) {
+            createdRight = await prisma.right.create({ data: { name: right } });
+        }
         allRights.push(createdRight);
     }
 
