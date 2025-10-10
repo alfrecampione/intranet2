9fc6e776f0fb1adb38a817e13dfbb1b4df47b0f2
Campione <80720402+alfrecampione@users.noreply.github.com>
2025-10-10 06:41:03 -0400

    lgo

--- src/controllers/reports.js
+++ src/controllers/reports.js
@@ -346,6 +346,8 @@ const exportData = async (req, res) => {
     try {
         const { headers = [], rows = [] } = req.body;
 
+        console.log(headers, rows)
+
         let csvContent = '';
         csvContent += headers.join(',') + '\n'; // Header row
         rows.forEach(row => {
