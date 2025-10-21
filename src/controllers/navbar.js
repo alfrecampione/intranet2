e3a6145a9c603b30b0b94bd8e36fda21590c7219
Campione <alfredo.montero@estudiantes.matcom.uh.cu>
2025-10-21 18:37:57 +0200

    logs

--- src/controllers/navbar.js
+++ src/controllers/navbar.js
@@ -80,6 +80,7 @@ const dataSearch = async (req, res) => {
 };
 
 const getNotifications = async (req, res) => {
+  console.log("Fetching notifications for user:", req.user);
   const userId = req.user.user_id;
 
   try {
