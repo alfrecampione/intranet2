7c85efa76fe7d5299619a0c666ebf741db0e53b5
Campione <80720402+alfrecampione@users.noreply.github.com>
2025-11-05 17:09:20 +0100

    change made in fetch

--- src/controllers/agents.js
+++ src/controllers/agents.js
@@ -282,7 +282,8 @@ const addAgent = async (req, res) => {
 };
 
 const onboardingSentEmail = async (req, res) => {
-  const { email } = req.body;
+  const { email } = req.params;
+  console.log("Onboarding email request for:", email);
   if (!email) {
     return res.status(400).json({ message: "Email is required." });
   }
